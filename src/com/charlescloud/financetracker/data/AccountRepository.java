c6bacdef16bb9e3f9b9840400d0c291e7403418b
charliecloud <charliecloud@gmail.com>
2016-03-21 14:31:11 -0400

    Removed commented lines

--- src/com/charlescloud/financetracker/data/AccountRepository.java
+++ src/com/charlescloud/financetracker/data/AccountRepository.java
@@ -26,33 +26,22 @@ public class AccountRepository {
         try {
             FileOutputStream fos = new FileOutputStream(fileName);
             ObjectOutputStream oos = new ObjectOutputStream(fos);
-//            for (Account a : ALL_ACCOUNTS){
-//                oos.writeObject(a);
-//            }
             oos.writeObject(ALL_ACCOUNTS);
         }catch(IOException ioe){
-            System.out.printf("Error saving file %s", fileName);
+            System.out.printf("Error saving file %s %n", fileName);
             ioe.printStackTrace();
         }
     }
 
     public void loadAccountsFromFilesystem(String fileName){
         if(!new File(fileName).exists()){
-            System.out.printf("File: %s does not exist", fileName);
+            System.out.printf("File: %s does not exist %n", fileName);
             return;
         }
         List<Account> accounts = new ArrayList<>();
         try{
             FileInputStream fis = new FileInputStream(fileName);
             ObjectInputStream ois = new ObjectInputStream(fis);
-//            try {
-//                while ((a = (Account) ois.readObject()) != null) {
-//                    addAccount(a);
-//                }
-//            }catch(ClassNotFoundException cnf){
-//                System.out.println("Error loading Account objects");
-//                cnf.printStackTrace();
-//            }
             try{
                 accounts = (List<Account>) ois.readObject();
             }catch(ClassNotFoundException cnf){
@@ -60,7 +49,7 @@ public class AccountRepository {
                 cnf.printStackTrace();
             }
         }catch(IOException ioe){
-            System.out.printf("Error opening file %s", fileName);
+            System.out.printf("Error opening file %s %n", fileName);
             ioe.printStackTrace();
         }
         ALL_ACCOUNTS.addAll(accounts);
