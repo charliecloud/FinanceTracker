d702df7fe89576ef57f722dbb028c591e39071ad
charliecloud <charliecloud@gmail.com>
2016-03-21 16:15:21 -0400

    Renaming

--- test/com/charlescloud/financetracker/AccountCalculatorTest.java
+++ test/com/charlescloud/financetracker/AccountCalculatorTest.java
@@ -6,18 +6,18 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AccountCalculatorTest {
-    AccountCalculator accounCalculator;
+    AccountCalculator accountCalculator;
 
     @Before
     public void setUp() throws Exception {
-        accounCalculator = new AccountCalculator();
+        accountCalculator = new AccountCalculator();
     }
 
     @Test
     public void earningsPercentageIsCorrect() throws Exception {
-        assertEquals(0,accounCalculator.calculateTotalEarningsPercentage(0,0),0);
-        assertEquals(100f, accounCalculator.calculateTotalEarningsPercentage(5,10),0);
-        assertEquals(-50f, accounCalculator.calculateTotalEarningsPercentage(10,5),0);
-        assertEquals(-100f,accounCalculator.calculateTotalEarningsPercentage(10,0),0);
+        assertEquals(0, accountCalculator.calculateTotalEarningsPercentage(0,0),0);
+        assertEquals(100f, accountCalculator.calculateTotalEarningsPercentage(5,10),0);
+        assertEquals(-50f, accountCalculator.calculateTotalEarningsPercentage(10,5),0);
+        assertEquals(-100f, accountCalculator.calculateTotalEarningsPercentage(10,0),0);
     }
 }
