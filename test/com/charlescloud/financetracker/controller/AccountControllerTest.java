667db663378139ce37421bcfc34659e7618d4442
charliecloud <charliecloud@gmail.com>
2016-03-21 14:34:58 -0400

    Removing commented lines

--- test/com/charlescloud/financetracker/controller/AccountControllerTest.java
+++ test/com/charlescloud/financetracker/controller/AccountControllerTest.java
@@ -17,7 +17,6 @@ public class AccountControllerTest {
 
     @Before
     public void setUp() throws Exception {
-        //account = new Account("test1", 1, false, 0.0f, 10f, true, AccountType.CD);
         accountController = new AccountController();
         accountController.createNewAccount("test1", "sample", 1, false, 0.0f, 10f, true, AccountType.CD);
 
@@ -25,14 +24,12 @@ public class AccountControllerTest {
 
     @Test
     public void findCorrectAccountUsingName() throws Exception {
-        //accountController.addAccount(account);
         assertEquals(accountController.getAllAccounts().get(0), accountController.getAccountByName("test1"));
 
     }
 
     @Test
     public void closingAccountMarksItClosed() throws Exception {
-        //accountController.addAccount(account);
         assertEquals(true, accountController.getAllAccounts().get(0).isOpen());
         accountController.markAccountAsClosed("test1");
         assertEquals(false, accountController.getAllAccounts().get(0).isOpen());
@@ -40,18 +37,14 @@ public class AccountControllerTest {
 
     @Test
     public void addingTransactionUpdatesBalanceCorrectly() throws Exception {
-        //accountController.addAccount(account);
-        accountController.addTransactionForAccount(new Transaction(new Date(),
+        accountController.addTransactionToAccount(new Transaction(new Date(),
                 TransactionType.BALANCE_UPDATE,50f,false),"test1");
         assertEquals(50f, accountController.getAllAccounts().get(0).getBalance(),0);
-        accountController.addTransactionForAccount(new Transaction(new Date(),
+        accountController.addTransactionToAccount(new Transaction(new Date(),
                 TransactionType.DIVIDEND_REINVEST,10f,true),"test1");
         assertEquals(60f, accountController.getAllAccounts().get(0).getBalance(),0);
-        accountController.addTransactionForAccount(new Transaction(new Date(),
+        accountController.addTransactionToAccount(new Transaction(new Date(),
                 TransactionType.CASH_OUT,5f,true),"test1");
         assertEquals(55f, accountController.getAllAccounts().get(0).getBalance(),0);
-        //Account account2 = new Account("test1", 1, false, 0.0f, 10f, true, AccountType.CD);
-
-
     }
 }
